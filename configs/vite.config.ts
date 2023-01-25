f8d4e3bda2d7a4e234cb18950f44298a0525e2fe
electrovir <electrovir@users.noreply.github.com>
2023-01-25 01:11:13 +0000

    update vite config for github pages base URL

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -1,6 +1,9 @@
 import {baseViteConfig} from 'virmator/dist/compiled-base-configs/base-vite';
 import {defineConfig} from 'vite';
 
+const prodConfig = {base: '/image-resizing-example/'};
+
 export default defineConfig({
     ...baseViteConfig,
+    ...(process.env.CI ? prodConfig : {}),
 });
