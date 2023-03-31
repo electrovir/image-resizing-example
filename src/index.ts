ae1ec4ac4f04d400f5b0f9f95327d57dfd30e9d0
electrovir <electrovir@users.noreply.github.com>
2023-03-31 13:40:13 +0000

    export inputs type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './core-package/augments/dimensions';
 export * from './core-package/resizable-image-data';
+export * from './core-package/vir-resizable-image/vir-resizable-image-inputs';
 export * from './core-package/vir-resizable-image/vir-resizable-image.element';
