4c6625a63c0630d8e1615f0400743fe1ca93645f
electrovir <electrovir@users.noreply.github.com>
2023-07-21 00:50:40 +0000

    remove iframe security stuff cause it didn't work anyway

--- src/core-package/iframe/resizable-image-frame.ts
+++ src/core-package/iframe/resizable-image-frame.ts
@@ -6,7 +6,6 @@ import {TemplateResult} from 'lit';
 import {ImageType, ResizableImageData} from '../image-data';
 import {textPadding} from '../style-constants';
 import {MessageType} from './iframe-messenger';
-import {removeLocalAccessScript} from './iframe-security';
 
 export function generateIframeDoc(
     imageData: ResizableImageData,
@@ -389,7 +388,6 @@ export function generateIframeDoc(
         <!DOCTYPE html>
         <html class="image-type-${imageData.imageType} ${allowScrolling ? 'allow-scrolling' : ''}">
             <head>
-                ${removeLocalAccessScript}
                 <style>
                     body,
                     html {
